5dff99d86cea7a7c596e35220bd071763aaf7027
Marcos Gamarra <marcos.gamarra12345@gmail.com>
2024-09-28 11:14:35 -0400

    hello

---
+++ java_setup.fish
@@ -0,0 +1,7 @@
+git clone https://github.com/nvim-java/nvim-java.git
+git clone https://github.com/nvim-java/nvim-java-core.git
+git clone https://github.com/nvim-java/nvim-java-dap.git
+git clone https://github.com/nvim-java/nvim-java-test.git
+git clone https://github.com/nvim-java/lua-async-await.git
+git clone https://github.com/MunifTanjim/nui.nvim.git
+git clone https://github.com/williamboman/mason.nvim.git
--- nvim/plugin/mini_indent.lua
+++
@@ -1,5 +0,0 @@
-require("mini.indentscope").setup(
-    {
-        symbol = "┃",
-    }
-)
